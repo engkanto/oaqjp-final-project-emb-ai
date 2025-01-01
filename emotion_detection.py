5ecb44fe31afb6b41b2b31ab487f2452df105a61
Fred Ricaforte <agilephp@gmail.com>
2025-01-02 01:28:03 +0800

    task 3

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    jsonfeed = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json=jsonfeed, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    # print(formatted_response)
+
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+
+    return emotion | {'dominant_emotion': max(emotion, key=emotion.get)}
